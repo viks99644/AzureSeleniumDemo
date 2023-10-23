179c43aa4ac5753b413a6d84ea50259a0ea58ac6
vikram <vikram.yavooo@gmail.com>
2023-10-23 06:43:56 +0530

    first git commit for my selenium  code

---
+++ src/test/java/mytests/HomePageTest.java
@@ -0,0 +1,48 @@
+package mytests;
+
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import ConfigReader.ConfigPropReader;
+import Factory.DriveFactory;
+import pages.HomePage;
+
+public class HomePageTest {
+	DriveFactory df;
+	ConfigPropReader cp;
+	Properties prop;
+	WebDriver driver;
+	HomePage homePage;
+	
+	
+
+	@BeforeTest
+	public void setup() {
+		cp = new ConfigPropReader();
+		prop = cp.initLangProp();
+		df = new DriveFactory();
+		driver = df.initDriver("chrome", prop);
+		homePage = new HomePage(driver);
+	}
+
+	@Test
+	public void headerTest() {
+		Assert.assertTrue(homePage.isHeaderExist(prop.getProperty("header")));
+	}
+
+	@Test
+	public void contactFooterTest() {
+		Assert.assertTrue(homePage.isContactExist(prop.getProperty("contact")));
+	}
+
+	@AfterTest
+	public void tearDown() {
+		driver.quit();
+	}
+
+}
